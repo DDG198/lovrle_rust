7d67998139d1bdc781790b754013e9037b288626
DDG198 <116456618+DDG198@users.noreply.github.com>
2024-05-06 12:18:18 +0100

    set location at correct times, improved output message

--- runner_script_bl_width.ps1
+++ runner_script_bl_width.ps1
@@ -5,19 +5,17 @@ For ($num_bikes = 0; $num_bikes -lt 401; $num_bikes+=50){
 For ($num_cars = 0; $num_cars -lt 401; $num_cars+=50) {
 
     Write-Output "setting up environment";
-    Set-Location "C:\Users\danie\Documents\Uni\4th Year\FYP\lovrle_rust_v2";
 
     $env:NUM_BIKES = $num_bikes;
     $env:NUM_CARS = $num_cars;
     $env:BL_WIDTH = $bl_width;
     $env:ML_WIDTH = 14 - $env:BL_WIDTH;
 
-    Set-Location "C:\Users\danie\Documents\Uni\4th Year\FYP\";
     $file_name = "c" + $env:NUM_CARS + "b" + $env:NUM_BIKES + "w" + $env:BL_WIDTH + ".json";
     $out_file = Join-Path -Path $data_dir -ChildPath $file_name;
     Write-Output "setting up environment";
 
-    Write-Output "outfile: " + $outfile;
+    Write-Output "outfile: " $outfile;
 
     if (Test-Path $out_file -PathType Leaf) {
         Write-Output "outfile already exists, skipping";
@@ -25,10 +23,12 @@ For ($num_cars = 0; $num_cars -lt 401; $num_cars+=50) {
     }
 
     Write-Output "building executable";
+    Set-Location "C:\Users\danie\Documents\Uni\4th Year\FYP\lovrle_rust_v2";
     cargo build --release;
     Write-Output "building complete";
 
     Write-Output "running executable";
+    Set-Location "C:\Users\danie\Documents\Uni\4th Year\FYP\";
     .\lovrle_rust_v2\target\release\lovrle_rust_v2.exe | Out-File -FilePath $out_file;
     Write-Output "executable complete";
 
